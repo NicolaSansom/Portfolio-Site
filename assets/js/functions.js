bad270049e850a21f1b21971e5982f61d2f955d6
NicolaSansom <nicolasansom1994@gmail.com>
2016-04-16 19:01:58 +0100

    Added in emojis on about me section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,57 @@
 
 $( document ).ready(function() {
+
+
+  var blinks = 0;
+  var timetotype = false;
+
+  while (blinks < 1) {
+  $('.typing-text').animate({"opacity": "0"}, "slow");
+  $('.typing-text').animate({"opacity": "1"}, "slow");
+  blinks = blinks + 1;
+  }
+  $('.typing-text').promise().done(function(){
+    $('.typing-text').html("Hello There!!");
+    var $el = $('.typing-text'),
+    html = $el.html(),
+    txt = $el.text(),
+    txtLen = html.length,
+    timeOut,
+    char = 0;
+
+$el.text('|');
+
+(function typeIt() {
+    var humanize = Math.round(Math.random() * (200 - 30)) + 30;
+    timeOut = setTimeout(function() {
+        char++;
+        var type = html.substring(0, char);
+        $el.html(type + '');
+        typeIt();
+
+        if (char == txtLen) {
+            $el.html($el.html().slice(0, -1)); // remove the '|'
+            clearTimeout(timeOut);
+        }
+
+    }, humanize);
+}());
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
     $('.mobile-nav-toggle').on('click', function() {
        $(".mobile-nav-toggle, .mobile-nav").toggleClass("is-open");
     });
